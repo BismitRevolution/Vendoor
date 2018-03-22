cd40780e5b730c3d1fd11e7a7d7dd4d0d46a4a65
nataprawiraf <nataprawiraf@gmail.com>
2018-03-23 02:00:35 +0700

    </form>

--- resources/views/vendors/edit.blade.php
+++ resources/views/vendors/edit.blade.php
@@ -1,52 +1,69 @@
 @extends('main')
 
+@section('extra-css')
+	<link rel="stylesheet" href="{{ asset('css/admin/admin.css') }}">
+@endsection
+
 @section('content')
+	
+	</form>
 
 	<div class="container">
-		<h1>A New Vendor Has Been Created</h1>
-		<h3>Vendor Spesification</h3>
 
+		<h1 id="header">Edit Vendor</h1>
 
-		<form action="{{ route('vendors.update', $vendor->vendor_id) }}" method="POST">
-	   		{{ csrf_field() }}
+		<form action="{{ route('admin.vendors.update', $vendor->vendor_id) }}" method="POST" class="form-settting">
+	   		
 	   		<input type="hidden" name="_method" value="PUT" />
 
+			{{ csrf_field() }}
+			
+            <label class="form-group has-float-label form-setting">
+            	<input id="name" name="name" for="name" class="form-control" type="text" value="{{ $vendor->name }}"/>
+                <span>Name</span>
+            </label>
 
-			<label for="">Vendor Name</label>
-			<input name="name" placeholder="{{ $vendor->name }}">
+            <label class="form-group has-float-label form-setting">
+                <input id="description" name="description" for="description" class="form-control" type="text" value="{{ $vendor->description }}"/>
+                <span>Description</span>
+            </label>
 
-			<label for="">Vendor Description</label>
-			<input name="description" placeholder="{{ $vendor->description }}">
+            <label class="form-group has-float-label form-setting">
+                <input id="address" name="address" for="address" class="form-control" type="text" value="{{ $vendor->address }}"/>
+                <span>Address</span>
+            </label>
 
-			<label for="">Vendor Address</label>
-			<input name="address" placeholder="{{ $vendor->address }}">
+            <label class="form-group has-float-label form-setting">
+                <input id="location_id" name="location_id" for="location_id" class="form-control" type="text" value="{{ $vendor->location_name }}"/>
+                <span>Location</span>
+            </label>
 
-			<label for="">Vendor Location</label>
-			<input name="location_id" placeholder="{{ $vendor->location_name }}">
+            <label class="form-group has-float-label form-setting">
+                <input id="category_id" name="category_id" for="category_id" class="form-control" type="text" value="{{ $vendor->category_name }}"/>
+                <span>Category</span>
+            </label>
 
-			<label for="">Vendor Category</label>
-			<input name="category_id" placeholder="{{ $vendor->category_name }}">
+            <label class="form-group has-float-label form-setting">
+                <input id="email" name="email" for="email" class="form-control" type="email" value="{{ $vendor->email }}"/>
+                <span>Email</span>
+            </label>
+			
+			<label class="form-group has-float-label form-setting">
 
-			<label for="">Vendor email</label>
-			<input name="email" type="email">
+                <input id="website" name="website" for="website" class="form-control" type="text" value="{{ $vendor->website }}"/>
+                <span>Website</span>
 
-			<label for="">Vendor phone</label>
-			<input name="phone">
+            </label>
+				
+			<button class="btn btn-primary btn-block btn-setting" type="submit">Edit</button>
 
-			<label for="">Vendor website</label>
-			<input name="website">
+    	</form>
 
-			<button type="submit">Update</button>
-		</form>
-
-		<form action="{{ route('vendors.destroy', ['id' => $vendor->vendor_id]) }}" method="POST">
+    	<form action="{{ route('admin.vendors.destroy', ['id' => $vendor->vendor_id]) }}" method="POST">
 	   		<input type="hidden" name="_method" value="DELETE" />
-	   		<button type="submit">Delete Vendor</button>
+	   		<button type="submit" class="btn btn-danger btn-block btn-setting">Delete Vendor</button>
 	    	{!! csrf_field() !!}
 		</form>
-
-		<small>dilihat sebanyak {{ $vendor->view_count }}</small>
-
 	</div>
 
 @endsection
