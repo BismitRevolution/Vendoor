5e40373b189d04786440d6bda03c4603a5123496
Nata <nataprawiraf@gmail.com>
2018-08-02 21:51:39 +0700

    added analytic to main

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -82,7 +82,7 @@
         <script async src="https://www.googletagmanager.com/gtag/js?id=UA-122674215-1"></script>
         <script>
           window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
+          function gtag(  ){dataLayer.push(arguments);}
           gtag('js', new Date());
 
           gtag('config', 'UA-122674215-1');
